87743e8bbadcfe677cf73537771ef3ce39f8e300
Richard <richardlwhittington@gmail.com>
2023-05-29 19:10:36 -0500

    add button

--- App.js
+++ App.js
@@ -6,10 +6,16 @@ import React from 'react';
 
 
 const App = () => {
-  
+  const clicky = ()=>{
+    alert("Hello World");
+  }
    return (
-<h1>Hello World</h1>
-  );
+    <>
+<h1 style={{color:"yellow"}}>Hello World</h1>
+<p>It's great to be here!</p>
+<button onClick={clicky}>Click Here</button>
+  </>
+   );
 };
 
 export default App;
