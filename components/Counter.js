2d35c222c87762ded757e3b4e794c6c6299098cb
Richard <richardlwhittington@gmail.com>
2023-06-03 18:30:27 -0500

    fix counter

--- components/Counter.js
+++ components/Counter.js
@@ -4,18 +4,19 @@ import React, { useState } from "react";
 const Counter = React.memo(() =>{
     const [count, setCount] = useState(0);
     
-    const add = () =>{
+    const add = (e) =>{
+        e.preventDefault();
         setCount(count + 1); 
     };
     const reset = () =>{
-        setCount();  
+        setCount(0);  
     };
     return(
         <>
         <div id="counting" style={{ height: '250px', width:'400px',margin:'auto', padding:'10px'}}>
        <h1>Counter</h1>
        <form>
-       <button className="btn btn-outline-dark" name="count" onClick={add}>Click To Add</button>
+       <button className="btn btn-outline-dark" name="count" type="button" onClick={add}>Click To Add</button>
        <div id="count">{count}</div>
        <input id="reset" type="button" value="Reset" onClick={reset} />
        </form>
